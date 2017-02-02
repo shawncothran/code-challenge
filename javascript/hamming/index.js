a928a7781b8b4e1d91f61c243fc72ae3923649cd
Shawn Cothran <shawncothran@Shawns-MacBook-Pro.local>
2017-02-02 01:29:02 -0600

    commit initial codebase and completed tests in python ans javascript

--- javascript/hamming/index.js
+++ javascript/hamming/index.js
@@ -1,7 +1,30 @@
-// Define your Hamming class here.
-// `npm test` to ensure the unit tests pass and
-// your code meets all of the conditions.
-// You may use ES6 or ES5 to solve.
+function Hamming() {};
 
+// Hamming.prototype.compute = function(strandA, strandB) {
+//   if (strandA.length === strandB.length) {
+//     var theNumberOfDifferencesBetweenTwoHomologousDNAStrandsTakenFromDifferentGenomesWithACommonAncestorThanksUncleBob = 0;
+//
+//     for (var i = 0; i < strandA.length; i++) {
+//       if (strandA[i] !== strandB[i]) {
+//         theNumberOfDifferencesBetweenTwoHomologousDNAStrandsTakenFromDifferentGenomesWithACommonAncestorThanksUncleBob++;
+//       }
+//     }
+//
+//     return theNumberOfDifferencesBetweenTwoHomologousDNAStrandsTakenFromDifferentGenomesWithACommonAncestorThanksUncleBob;
+//   }
+//
+//   throw 'DNA strands must be of equal length.';
+// };
+
+Hamming.prototype.compute = function(strandA, strandB) {
+  // full disclosure: I had to google how to create prototype methods on ES5 classes because I hadn't done it in over a year
+  if (strandA.length === strandB.length) {
+    var dist = strandA.split('').filter(function(el, i) {return el !== strandB[i]});
+
+    return dist.length;
+  }
+
+  throw 'DNA strands must be of equal length.';
+};
 
 module.exports = Hamming;
--- python/hamming/__init__.py
+++ python/hamming/__init__.py
@@ -3,5 +3,20 @@
 # unit tests pass and your code meets all of the conditions.
 #
 
-def compute():
-    pass
+def compute(strandA, strandB):
+    if len(strandA) == len(strandB):
+        distance = 0
+        for i in range(len(strandA)):
+            if strandA[i] != strandB[i]:
+                distance += 1
+        return distance
+    raise ValueError('DNA strands must be of equal length.')
+
+# what am i doing wrong down below?
+# def compute(strandA, strandB):
+#     if len(strandA) == len(strandB):
+#         for i, char in enumerate(strandA):
+#             if char == strandB[i]:
+#                 strandA = strandA[:i] + strandA[i + 1:]
+#         return len(strandA)
+#     raise ValueError('DNA strands must be of equal length.')
